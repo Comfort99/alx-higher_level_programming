7977565e5cf48b1f9b1eacde7cde75c54c78581f
Comfort99 <shingangecomfort@gmail.com>
2024-01-03 12:08:00 -0800

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,17 +8,29 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	/* Create a new node */
-	listint_t *new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{/* Allocation failed */
+	listint_t *new_node;
+	listint_t *current;
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)  /* If memory allocation failed, return NULL */
 		return (NULL);
+	new_node->n = number;  /* Assign the data to the new node */
+	new_node->next = NULL;  /* Initialize the next pointer of new node to NULL */
+	/* If the list is empty or the new node should be inserted before the head node */
+	if (*head == NULL || (*head)->n >= new_node->n)
+	{
+		new_node->next = *head;  /* Make the new node point to the current head node */
+		*head = new_node;  /* Update the head pointer to point to the new node */
 	}
-	/* Set the values for the new node */
-	new_node->n = number;
-	new_node->next = *head;
-	/* Update the head to point to the new node */
-	*head = new_node;
-	/* Return the address of the new node */
-	return (new_node);
+	else
+	{/* Find the node before the point of insertion */
+		current = *head;
+		while (current->next != NULL && current->next->n < new_node->n)
+		{
+			current = current->next;
+		}/* Insert the new node */
+		new_node->next = current->next;
+		current->next = new_node;
+	}
+	return (new_node);  /* Return the new node */
 }
