0bd55e0fc8037bc7255009f53bb2e6ba1e728bf8
Comfort99 <shingangecomfort@gmail.com>
2024-01-04 06:12:26 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if num_args < 1:
         print("{} argurments".format(num_args))
     elif num_args == 1:
-        print("{} arguments".format(num_args))
+        print("{} arguments:".format(num_args))
     else:
-        print("{} arguments".format(num_args))
-    for i in range(1, num_args + 1):
+        print("{} arguments:".format(num_args))
+    for i in range(1,num_args + 1):
         print("{}: {}".format(i, argv[i]))
