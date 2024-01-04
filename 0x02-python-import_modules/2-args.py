7ddb6f3c99858cc462c731ed602577fb2e6dcd9d
Comfort99 <shingangecomfort@gmail.com>
2024-01-04 07:33:40 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     num_args = len(argv) - 1
     if num_args < 1:
         print("{} arguments.".format(num_args))
-    elif x == 1:
+    elif num_args == 1:
         print("{} argument:".format(num_args))
     else:
         print("{} arguments:".format(num_args))
