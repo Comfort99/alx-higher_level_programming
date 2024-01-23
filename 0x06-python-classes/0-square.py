972c57ef9576fd4757bb41c5aecdbb0587da7300
Comfort99 <shingangecomfort@gmail.com>
2024-01-23 06:39:53 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-
+# Define a class named Square
 class Square:
+    # The pass statement is a placeholder and means "do nothing"
+    # It's used here because we don't want to put anything in the class yet
     pass
