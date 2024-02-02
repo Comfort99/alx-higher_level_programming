49bc6c441fc35ca0d0862b0960e8e943b4fb1636
Comfort99 <shingangecomfort@gmail.com>
2024-02-02 11:29:32 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,16 +26,34 @@ def matrix_divided(matrix, div):
 
     """
 
-    typeErr = "matrix must be a matrix (list of lists) of integers/floats"
-    sizeErr = "Each row of the matrix must have the same size"
-    for row in matrix:
-        if len(element) != len(matrix[0]):
-            raise TypeError(sizeErr)
-        for element in row:
-            if not isinstance(y, (int, float)):
-                raise TypeError(typeErr)
-    if not (isinstance(div, (int, float))):
+    if not type(div) in (int, float):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(element / div, 2) for element in row] for row in matrix]
+
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
+
+    len_e = 0
+    msg_size = "Each row of the matrix must have the same size"
+
+    for elems in matrix:
+        if not elems or not isinstance(elems, list):
+            raise TypeError(msg_type)
+
+        if len_e != 0 and len(elems) != len_e:
+            raise TypeError(msg_size)
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError(msg_type)
+
+        len_e = len(elems)
+
+     nm = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (nm)
+
+    
