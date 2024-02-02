bb8864f3ec68e14af1dc8d592b0dd47b2f708795
Comfort99 <shingangecomfort@gmail.com>
2024-02-02 14:47:16 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,34 +26,26 @@ def matrix_divided(matrix, div):
 
     """
 
-    if not type(div) in (int, float):
-        raise TypeError("div must be a number")
+    typeerr = "matrix must be a matrix (list of lists) of integers/floats"
+    typesize = "Each row of the matrix must have the same size"
+    if not isinstance(matrix, list):
+        raise TypeError(typeerr)
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(typesize)
+
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(typeerr)
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError(typesize)
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if not matrix or not isinstance(matrix, list):
-        raise TypeError(msg_type)
-
-    len_e = 0
-    msg_size = "Each row of the matrix must have the same size"
-
-    for elems in matrix:
-        if not elems or not isinstance(elems, list):
-            raise TypeError(msg_type)
-
-        if len_e != 0 and len(elems) != len_e:
-            raise TypeError(msg_size)
-
-        for num in elems:
-            if not type(num) in (int, float):
-                raise TypeError(msg_type)
-
-        len_e = len(elems)
-
-     nm = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-    return (nm)
-
-    
+    return [[round(element / div, 2) for element in row] for row in matrix]
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,17 +4,18 @@
 Function that prints a sqaure of the character
 """
 
+
 def print_square(size):
     """
     Arg:
         size: size of the square
-    
+
     check size if is an integer:
                 raise TypeError(size must be an integer)
-    
+
     if size is less than 0:
                 raise ValueError(size must be greater or equal to 0)
-    
+ 
     Print the output
     """
 
