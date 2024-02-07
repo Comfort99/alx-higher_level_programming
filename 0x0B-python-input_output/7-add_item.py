507d359819b12636b7682dfb4673958f701b70a5
Comfort99 <shingangecomfort@gmail.com>
2024-02-06 23:52:33 -0800

    modified

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ Module Input/Output
 
 import sys
 import json
-import os
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
