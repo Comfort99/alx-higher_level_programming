de1cb053edaf266d1a388e353c7fd98c59b2895e
Comfort99 <shingangecomfort@gmail.com>
2024-04-13 06:42:06 -0700

    py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that lists all states
+with a name starting with N (upper N) 
+from the database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost")
+    cur = db.cursor()
+    create_name_query = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id"
+    cur.execute(create_name_query)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
