c792550d0821dfd4c17f3582ff9190c883c10a4e
Comfort99 <shingangecomfort@gmail.com>
2024-04-25 11:25:58 -0700

    modified

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
-
-if [ $# -lt 1 ]
-then
-	echo "no argument passed"
-else
-	url=$1
-	curl -s "$url" | wc -c
-fi
+# and displays the size of the body of the respon
+curl -s -w "%{SIZE_DOWNLOAD}" -o /dev/null $1
